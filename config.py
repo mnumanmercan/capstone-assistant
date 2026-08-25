65a5879900cc8aaf8730d3086185c829b2a937b1
Numan <numan.mercan24@gmail.com>
2026-08-25 22:59:30 +0300

    init: config file created and builded

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent / ".env",
+    )
 
     anthropic_api_key: str
     voyage_api_key: str
-    
+    max_tokens: int = 1024
 
 settings = Settings()
